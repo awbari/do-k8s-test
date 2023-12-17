47d1a305c96d1d56ec34a86f674a9a1fdf3180c1
Jan Stümmel <7893682+janstuemmel@users.noreply.github.com>
2023-12-17 14:27:10 +0100

    fix terraform fmt

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -28,17 +28,15 @@ provider "github" {
 }
 
 provider "kubernetes" {
-  host  = digitalocean_kubernetes_cluster.primary.endpoint
-  token = digitalocean_kubernetes_cluster.primary.kube_config[0].token
-  cluster_ca_certificate = base64decode(
-    digitalocean_kubernetes_cluster.primary.kube_config[0].cluster_ca_certificate
-  )
+  host                    = digitalocean_kubernetes_cluster.primary.endpoint
+  token                   = digitalocean_kubernetes_cluster.primary.kube_config[0].token
+  cluster_ca_certificate  = base64decode(digitalocean_kubernetes_cluster.primary.kube_config[0].cluster_ca_certificate)
 }
 
 provider "flux" {
   kubernetes = {
-    token = digitalocean_kubernetes_cluster.primary.kube_config[0].token
     host                   = digitalocean_kubernetes_cluster.primary.endpoint
+    token                  = digitalocean_kubernetes_cluster.primary.kube_config[0].token
     cluster_ca_certificate = base64decode(digitalocean_kubernetes_cluster.primary.kube_config[0].cluster_ca_certificate)
   }
   git = {
