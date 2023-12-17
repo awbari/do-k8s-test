080145202c9c257bb289cfa2dd870253b4c4e3b0
Jan Stümmel <7893682+janstuemmel@users.noreply.github.com>
2023-12-17 15:23:39 +0100

    set replicas to 2 for whoami deployment

--- infrastructure/flux.tf
+++ infrastructure/flux.tf
@@ -11,20 +11,11 @@ resource "github_repository_deploy_key" "this" {
   read_only  = "false"
 }
 
-resource "kubernetes_namespace" "flux_system" {
-  metadata {
-    name = "flux-system"
-  }
-
-  lifecycle {
-    ignore_changes = [
-      metadata[0].labels,
-      # metadata[0].annotations, # TODO: need to check if this one can be safely ignored
-    ]
-  }
-}
-
 resource "flux_bootstrap_git" "this" {
   depends_on = [github_repository_deploy_key.this]
   path = "k8s"
+  components = [
+    "source-controller",
+    "kustomize-controller",
+  ]
 }
